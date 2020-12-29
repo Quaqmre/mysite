5dc3394c4d9abcfdcf9a1763a12fd6ef5611ceff
Quaqmre <tatarmehmetakif@gmail.com>
2020-12-29 23:46:17 +0300

    a

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,53 @@
+# #!/bin/sh
+
+# # If a command fails then the deploy stops
+# set -e
+
+# printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+# # Build the project.
+# hugo
+
+# # Go To Public folder
+# cd public
+
+# # Add changes to git.
+# git add .
+
+# # Commit changes.
+# msg="rebuilding site $(date)"
+# if [ -n "$*" ]; then
+# 	msg="$*"
+# fi
+# git commit -m "$msg"
+
+# # Push source and build repos.
+# git push origin main
 #!/bin/sh
 
-# If a command fails then the deploy stops
-set -e
+if [ "`git status -s`" ]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
 
-printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+echo "Deleting old publication"
+rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
 
-# Build the project.
+echo "Checking out gh-pages branch into public"
+git worktree add -B gh-pages public upstream/gh-pages
+
+echo "Removing existing files"
+rm -rf public/*
+
+echo "Generating site"
 hugo
 
-# Go To Public folder
-cd public
+echo "Updating gh-pages branch"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
 
-# Add changes to git.
-git add .
-
-# Commit changes.
-msg="rebuilding site $(date)"
-if [ -n "$*" ]; then
-	msg="$*"
-fi
-git commit -m "$msg"
-
-# Push source and build repos.
-git push origin main
+#echo "Pushing to github"
+#git push --all
